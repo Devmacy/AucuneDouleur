7ab2ff78b6bde7d607ad43f5032ecf983adbc173
Devmacy <devmaccyy@gmail.com>
2023-01-16 02:06:36 +0800

    fix: fix eslint can not check ts file
    add new property in parserOptions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,21 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'vue'
-  ],
-  rules: {
-  }
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        'standard-with-typescript'
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json'],
+    },
+    plugins: [
+        'vue'
+    ],
+    rules: {}
 }
