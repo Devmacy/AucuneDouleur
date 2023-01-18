4ba65c874ea5e92d76a5e733cbfc4092aa411477
Devmacy <devmaccy@gmail.com>
2023-01-18 17:48:40 +0800

    build: 新增ElementUI Plus

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
+    <el-button>Default</el-button>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import './style.css'
 import App from './App.vue'
 
 const pinia = createPinia()
 const app = createApp(App)
 
-app.use(pinia)
-
-createApp(App).mount('#app')
+app.use(pinia).use(ElementPlus).mount('#app')
