665dc7ba91b6eb623327e9f674b7b2b0e420b95e
Devmacy <devmaccy@gmail.com>
2023-01-18 17:36:53 +0800

    build: 新增pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
+
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
 
 createApp(App).mount('#app')
