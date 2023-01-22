f23ad69ac6c01ad06993cc8a70e8f1bdfb8e7f0a
Devmacy <devmaccyy@gmail.com>
2023-01-22 17:12:04 +0800

    feat: 新增主题切换

--- src/components/MyHeaders.vue
+++ src/components/MyHeaders.vue
@@ -3,6 +3,15 @@ import { toggleDark } from '@/darkTheme'
 import { ref } from 'vue'
 
 const isNight = ref(false)
+
+function loadStyles (url) {
+  const link = document.createElement('link')
+  link.rel = 'stylesheet'
+  link.type = 'text/css'
+  link.href = url
+  const head = document.getElementsByTagName('head')[0]
+  head.appendChild(link)
+}
 </script>
 
 <template>
@@ -13,6 +22,8 @@ const isNight = ref(false)
       inactive-icon="Sunny"
       @change="toggleDark"
   />
+
+  <el-button @click="loadStyles('test.css')">change</el-button>
 </template>
 
 <style scoped>
