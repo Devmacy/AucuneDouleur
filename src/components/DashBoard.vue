55c98e5e6108aab3e1cbd7d2185017c6de3f6d89
Devmacy <devmaccyy@gmail.com>
2023-01-22 15:30:49 +0800

    feat: 新增自定义主题

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -2,8 +2,12 @@
 </script>
 
 <template>
-  <div>dashboard</div>
-  <el-button type="default">default</el-button>
+  <el-button>Default</el-button>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+  <el-button type="info">Info</el-button>
+  <el-button type="warning">Warning</el-button>
+  <el-button type="danger">Danger</el-button>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import '@/styles/element/index.scss'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import '@/style.css'
 import App from '@/App.vue'
 import router from '@/router'
