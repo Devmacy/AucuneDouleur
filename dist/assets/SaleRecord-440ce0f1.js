2160858d4a436cb0f2847724941e556e69262bb2
Devmacy <devmaccyy@gmail.com>
2023-01-31 00:24:20 +0800

    refactor: 更改gitignore

--- dist/assets/SaleRecord-440ce0f1.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-17f86cfb.js";const r={};function c(t,_){return" 哇偶宠物店单据录入 "}const o=e(r,[["render",c]]);export{o as default};
--- dist/assets/SelectColor-8b45e164.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-17f86cfb.js";const r={};function c(t,o){return" 颜色 "}const n=e(r,[["render",c]]);export{n as default};
