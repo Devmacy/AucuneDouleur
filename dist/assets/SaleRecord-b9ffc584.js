265f232741ec1b7f83591239ccef8bf9d9ef5b38
Devmacy <devmaccy@gmail.com>
2023-01-30 16:31:42 +0800

    build: build

---
+++ dist/assets/SaleRecord-b9ffc584.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-82f72e09.js";const r={};function c(t,_){return"123"}const o=e(r,[["render",c]]);export{o as default};
---
+++ dist/assets/SelectColor-c8730bbf.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-82f72e09.js";const r={};function c(t,o){return" 颜色 "}const n=e(r,[["render",c]]);export{n as default};
