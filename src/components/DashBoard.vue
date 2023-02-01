c71b27ec94e8cdf7047e2fb2cb52c25ac852a086
Devmacy <devmaccyy@gmail.com>
2023-02-02 01:33:15 +0800

    fix(dashboard): 新增首页

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -1,16 +1,54 @@
 <script setup lang="ts">
+import WeatherShow from "@/components/customDashboard/WeatherShow.vue";
+import Sortable from 'sortablejs'
+import {onMounted} from "vue";
+
+onMounted(() => {
+  const container = document.getElementById('main-wrapper') as HTMLElement
+  Sortable.create(container, {
+    animation: 180,
+    delay: 10,
+  })
+})
+
+const bodyStyle = {
+  "padding":0
+}
 </script>
 
 <template>
-  <el-button>Default</el-button>
-  <el-button type="primary">Primary</el-button>
-  <el-button type="success">Success</el-button>
-  <el-button type="info">Info</el-button>
-  <el-button type="warning">Warning</el-button>
-  <el-button type="danger">Danger</el-button>
+  <div class="main-wrapper" id="main-wrapper">
 
-  <el-pagination :total="100" />
+    <el-card class="item" shadow="hover" :body-style="bodyStyle" v-for="i in 13" :key="i">
+      <weather-show/>
+    </el-card>
+
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.main-wrapper {
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-row-gap: 20px;
+  grid-column-gap: 20px;
+  justify-items: center;
+  align-items: center;
+  justify-content: space-evenly;
+
+  .item {
+    width: 500px;
+    height: 280px;
+    padding: 0;
+  }
+}
+
+/* 滚动条整体 */
+.main-wrapper::-webkit-scrollbar {
+  display: none;
+}
+
 </style>
---
+++ src/components/customDashboard/WeatherShow.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="main-container">
+    {{Math.random()}}
+  </div>
+</template>
+
+<style scoped lang="scss">
+.main-container{
+  width: 100%;
+  height: 100%;
+}
+</style>
