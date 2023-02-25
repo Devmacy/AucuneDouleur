fc433d4288de0f5989fc5d84e8f071106dd5718c
Devmacy <devmaccyy@gmail.com>
2023-02-25 21:18:15 +0800

    feat(颜色选择): 新增颜色滑块

--- src/views/SelectColor.vue
+++ src/views/SelectColor.vue
@@ -2,11 +2,17 @@
 
 import {reactive} from "vue";
 import {getRandomNumberLR} from "@/utils/number/randomNumbe";
+import {RGBAStrToList} from "@/utils/color";
 
 const colorState = reactive({
   selectedColor: '',
   colorList: [] as Array<Array<string>>,
   listCount: 10,
+
+  slideR: 0,
+  slideG: 0,
+  slideB: 0,
+  slideA: 0
 })
 
 
@@ -41,7 +47,48 @@ const generateColorList = (type: string, count?: number): void => {
  * @param rgbColorList
  */
 const setCurrentColor = (rgbColorList: Array<string>): void => {
-  colorState.selectedColor = `rgba(${rgbColorList[0]},${rgbColorList[1]},${rgbColorList[2]},${rgbColorList[3]})`
+  const R = rgbColorList[0]
+  const G = rgbColorList[1]
+  const B = rgbColorList[2]
+  const A = rgbColorList[3]
+  colorState.slideR = parseInt(R)
+  colorState.slideG = parseInt(G)
+  colorState.slideB = parseInt(B)
+  colorState.slideA = parseInt(A)
+  colorState.selectedColor = `rgba(${R},${G},${B},${A})`
+}
+
+/**
+ * 滑块change事件
+ * @param value 值
+ * @param type 滑块类型
+ */
+const slideChange = (value: number, type: string) => {
+  type === 'R' ? colorState.slideR = value : ''
+  type === 'G' ? colorState.slideG = value : ''
+  type === 'B' ? colorState.slideB = value : ''
+  type === 'A' ? colorState.slideA = value : ''
+  colorState.selectedColor = `rgba(${colorState.slideR},${colorState.slideG},${colorState.slideB},${(colorState.slideA / 100).toFixed(2)})`
+}
+
+/**
+ * 颜色选择器change事件
+ * @param value
+ */
+const colorPickerChange = (value: string) => {
+  const [slideR, slideG, slideB, slideA] = RGBAStrToList(value) as Array<string>
+  colorState.slideR = parseInt(slideR)
+  colorState.slideG = parseInt(slideG)
+  colorState.slideB = parseInt(slideB)
+  colorState.slideA = parseFloat(slideA) * 100
+}
+
+/**
+ * 格式化滑块
+ * @param val 值
+ */
+const formatAlpha = (val: number) => {
+  return val / 100
 }
 
 </script>
@@ -62,12 +109,23 @@ const setCurrentColor = (rgbColorList: Array<string>): void => {
         </template>
 
         <template #append>
-          <el-color-picker show-alpha color-format="rgb" v-model="colorState.selectedColor"/>
+          <el-color-picker show-alpha color-format="rgb" v-model="colorState.selectedColor"
+                           @change="colorPickerChange"/>
         </template>
       </el-input>
 
-      <div class="current-color-container">
-        <div class="current-color" :style="{backgroundColor:colorState.selectedColor}"></div>
+      <div class="flex-row-ac">
+        <div class="current-color-container">
+          <div class="current-color" :style="{backgroundColor:colorState.selectedColor}"></div>
+        </div>
+
+        <div class="flex-col" style="flex: 1;padding: 0 10px 0  20px">
+          <el-slider v-model="colorState.slideR" :max="255" @change="slideChange($event,'R')"/>
+          <el-slider v-model="colorState.slideG" :max="255" @change="slideChange($event,'G')"/>
+          <el-slider v-model="colorState.slideB" :max="255" @change="slideChange($event,'B')"/>
+          <el-slider v-model="colorState.slideA" :max="100" :format-tooltip="formatAlpha"
+                     @change="slideChange($event,'A')"/>
+        </div>
       </div>
 
     </el-card>
