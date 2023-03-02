70c82bfad1da2508cb065daf88d3b256f9c28e4b
Devmacy <devmaccy@gmail.com>
2023-03-02 17:48:15 +0800

    build(首页): 开启GZIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression'
 
 import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        viteCompression({
+            threshold: 1024 // 对大于 100kb 的文件进行压缩
+        })
+    ],
 
     //  配置别名
     resolve: {
